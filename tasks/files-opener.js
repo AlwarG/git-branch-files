4baf33c790d5362107cd1b1ffc312caaec62e1b8
alwar.g <alwar.g@zohocorp.com>
2021-07-05 17:15:34 +0530

    Adding Info

--- tasks/files-opener.js
+++ tasks/files-opener.js
@@ -2,12 +2,16 @@ const getFilePaths = require('./get-file-paths');
 const openFiles = require('./open-files');
 
 module.exports = function filesOpener(vscode) {
+   let {
+      source_branch: defaulBranch
+   } = vscode.workspace.getConfiguration()['git-branch-files'] || {};
+   
    getFilePaths({
       vscode,
-      defaulBranch: 'master'
+      defaulBranch: defaulBranch || 'master'
    }).then((paths) => {
       openFiles({ vscode, paths }).then(() => {
-         vscode.window.showInformationMessage('Files have been opened');
+         vscode.window.showInformationMessage('Your files have been opened');
       });
    }).catch((err) => {
       if (err && err.errorMsg) {
